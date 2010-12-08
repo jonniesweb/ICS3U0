a9a2c9762f9e751360fa4638bd3ec50a50b22f4f
Jonnie <Jonnie@Jonnie-Netbook>
2010-12-08 13:27:55 -0500

    Implemented the way it was reversed using the teachers way
    Put in another way to accomplish the reversing and asked the user to choose which one they wanted to use

--- src/String_reverse.java
+++ src/String_reverse.java
@@ -18,17 +18,33 @@ public class String_reverse {
 	public static void main (String[] args)
 	{
 		c = new Console (25, 80, 14, "String Reverser");
-		
+
+		c.print("Please enter y to use the StringBuffer way or n to use the old way: ");
+		char type = c.readChar();
+
+
 		// Get the text that will be reversed
 		c.println("This program reverses whatever you type in.");
 		c.print("Enter a string: ");
 		String text = c.readLine();
-		
-		// Use this handy dandy object called StringBuffer to reverse the text
-		StringBuffer reverse = new StringBuffer(text).reverse();
+
+		if (type == 'y') {
+			// Use this handy dandy object called StringBuffer to reverse the text
+			StringBuffer reverse = new StringBuffer(text).reverse();
+			c.println("In the reverse the string is: " + reverse);
+
+		} else if (type == 'n') {
+
+			String reverse = "";
+			for (int i = text.length() - 1; i >= 0; i--) {
+
+				reverse += text.charAt(i);
+			}
+			c.println("In the reverse the string is: " + reverse);
+		}
 
 		// Print to the console the reversed string
-		c.println("In the reverse the string is: " + reverse);
+
 
 	} // main method
 } // test class
